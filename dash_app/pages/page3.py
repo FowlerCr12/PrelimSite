2be03165a2a621aacad32817020edfc81bd6cce0
FowlerCr12 <cafowlerjr@crimson.ua.edu>
2025-01-26 21:31:12 -0600

    awd

--- dash_app/pages/page3.py
+++ dash_app/pages/page3.py
@@ -8,7 +8,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import pymysql
 
-dash.register_page(__name__, path="/stats")
+dash.register_page(__name__, path="/page3")
 
 def get_claims_data():
     conn = get_db_connection()
