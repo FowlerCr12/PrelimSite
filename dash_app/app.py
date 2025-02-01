0e250bb80fe8c215b055d32016d4fae7b5f46866
FowlerCr12 <cafowlerjr@crimson.ua.edu>
2025-01-31 18:04:24 -0600

    added cnc logo

--- dash_app/app.py
+++ dash_app/app.py
@@ -83,7 +83,7 @@ header = dmc.AppShellHeader(
     dmc.Group(
         [
             dmc.Burger(id="burger", size="sm", hiddenFrom="sm", opened=False),
-            dmc.Image(src="https://cdn.prod.website-files.com/6127cf52b9d335ca60c65756/6193e8b279a934f3cbd246a4_CNC.svg", width=100),
+            dmc.Image(src="/dash/assets/cnclogo.png", w=50, h=50, ml=10, mr=10),
             dmc.Title("Status Report Program", c="blue"),
         ],
         h="100%",
